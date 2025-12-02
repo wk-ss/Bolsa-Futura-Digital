3faff99bade29580ae740df56cf3b922173b11fd
Willians k. <54045082+wk-ss@users.noreply.github.com>
2025-12-02 14:51:12 -0300

    Atividade
    Colocando em pratica criar uma api e salvar no banco mysql

---
+++ Modulo03/Api-cep/api_cep.py
@@ -0,0 +1,54 @@
+import requests
+import mysql.connector
+
+
+conexao = mysql.connector.connect(
+    host="localhost", user="root", password="12345678", database="cep"
+)
+if conexao.is_connected():
+    print("Finalmente conectou")
+cursor = conexao.cursor()
+
+# cep = input("digite o seu cep:")
+cep = "50610130"
+cep = cep.replace("-", "").replace(".", "").replace(" ", "")
+link = f"https://viacep.com.br/ws/{cep}/json/"
+requisicao = requests.get(link)
+dados = requisicao.json()
+
+"""for coluna,descricao in dados.items():
+    print(f"{coluna}: {descricao}")
+
+    OU
+
+print(f"CEP: {dados['cep']}")
+print(f"Logradouro: {dados['logradouro']}")
+print(f"Bairro: {dados['bairro']}")
+print(f"Localidade: {dados['localidade']}")
+print(f"UF: {dados['uf']}")
+"""
+
+sql = """
+INSERT INTO endereco (cep, logradouro, bairro, localidade, uf)
+VALUES (%s, %s, %s, %s, %s)
+"""
+valores = (
+    dados["cep"],
+    dados["logradouro"],
+    dados["bairro"],
+    dados["localidade"],
+    dados["uf"],
+)
+
+cursor.execute(sql, valores)
+conexao.commit()
+
+
+cursor.execute("SELECT * FROM endereco")
+resultado = cursor.fetchall()
+
+for linha in resultado:
+    print(linha)
+
+cursor.close()
+conexao.close()
